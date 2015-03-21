fa91ef9f60ac0d14f0f22e4a6e6f86a6bb13726d
Darren Liu <butterfly@msrlab.org>
2015-03-21 20:23:35 +0800

    Updated UI.

--- WeCenterMobile/Controller/QuestionPublishmentViewController.swift
+++ WeCenterMobile/Controller/QuestionPublishmentViewController.swift
@@ -8,40 +8,39 @@
 
 import UIKit
 
-class QuestionPublishmentViewController: UIViewController, ZFTokenFieldDataSource, ZFTokenFieldDelegate {
+class QuestionPublishmentViewController: UIViewController, ZFTokenFieldDataSource, ZFTokenFieldDelegate, UICollectionViewDataSource, UICollectionViewDelegate {
     
     @IBOutlet weak var dismissButton: UIButton!
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var tagsField: ZFTokenField!
+    @IBOutlet weak var imageCollectionView: UICollectionView!
+    @IBOutlet weak var imageCollectionViewHeightConstraint: NSLayoutConstraint!
+    
+    let identifier = "ImageCell"
     
     var tags = [String]()
+    var images = [UIImage]()
     
     override func awakeFromNib() {
         super.awakeFromNib()
         dismissButton.addTarget(self, action: "dismiss", forControlEvents: .TouchUpInside)
         scrollView.alwaysBounceVertical = true
+        tagsField.textField.textColor = UIColor.lightTextColor()
+        tagsField.textField.font = UIFont.systemFontOfSize(14)
+        tagsField.textField.attributedPlaceholder = NSAttributedString(string: "...", attributes: [NSForegroundColorAttributeName: UIColor.lightTextColor().colorWithAlphaComponent(0.3)])
+        imageCollectionView.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: identifier)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tagsField.delegate = self
-        tagsField.dataSource = self
-        tagsField.textField.textColor = UIColor.lightTextColor()
-        tagsField.textField.font = UIFont.systemFontOfSize(14)
-        tagsField.textField.attributedPlaceholder = NSAttributedString(string: "...", attributes: [NSForegroundColorAttributeName: UIColor.lightTextColor().colorWithAlphaComponent(0.3)])
         tagsField.reloadData()
-    }
-    
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
-//        tagsField.textField.textColor = UIColor.lightTextColor()
-//        tagsField.textField.attributedPlaceholder = NSAttributedString(string: "...", attributes: [NSForegroundColorAttributeName: UIColor.lightTextColor().colorWithAlphaComponent(0.3)])
+        imageCollectionView.reloadData()
     }
     
     // MARK: - ZFTokenFieldDataSource
     
     func lineHeightForTokenInField(tokenField: ZFTokenField!) -> CGFloat {
-        return 33
+        return 24
     }
     
     func numberOfTokenInField(tokenField: ZFTokenField!) -> UInt {
@@ -55,6 +54,10 @@ class QuestionPublishmentViewController: UIViewController, ZFTokenFieldDataSourc
         label.font = UIFont.systemFontOfSize(14)
         label.sizeToFit()
         label.frame.size.height = lineHeightForTokenInField(tagsField)
+        label.frame.size.width += 20
+        label.textAlignment = .Center
+        label.layer.masksToBounds = true
+        label.layer.cornerRadius = 3
         label.backgroundColor = UIColor.lightTextColor()
         return label
     }
@@ -72,6 +75,32 @@ class QuestionPublishmentViewController: UIViewController, ZFTokenFieldDataSourc
     func tokenField(tokenField: ZFTokenField!, didReturnWithText text: String!) {
         tags.append(text)
         tagsField.reloadData()
+    }
+    
+    // MARK: - UICollectionDataSource
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        println(__FUNCTION__)
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        println(__FUNCTION__)
+        return 10 // images.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        println(__FUNCTION__)
+        NSLog("%@", indexPath)
+        let cell = imageCollectionView.dequeueReusableCellWithReuseIdentifier(identifier, forIndexPath: indexPath) as! UICollectionViewCell
+        cell.backgroundColor = UIColor.msr_randomColor(opaque: true)
+        return cell
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        imageCollectionView.reloadData()
+        imageCollectionViewHeightConstraint.constant = imageCollectionView.contentSize.height
     }
     
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
