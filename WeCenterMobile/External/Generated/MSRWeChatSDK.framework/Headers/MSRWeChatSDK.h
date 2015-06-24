161d69c36b19a15b8a7e8262d1f1fac131267dc2
Darren Liu <butterfly@msrlab.org>
2015-06-24 14:32:08 +0800

    Changed MSRWeChatSDK from sub-target(submodule) to independent framework.

---
+++ WeCenterMobile/External/Generated/MSRWeChatSDK.framework/Headers/MSRWeChatSDK.h
@@ -0,0 +1,19 @@
+//
+//  MSRWeChatSDK.h
+//  MSRWeChatSDK
+//
+//  Created by Darren Liu on 15/4/14.
+//  Copyright (c) 2015年 MsrLab. All rights reserved.
+//
+
+@import Foundation;
+
+//! Project version number for MSRWeChatSDK.
+FOUNDATION_EXPORT double MSRWeChatSDKVersionNumber;
+
+//! Project version string for MSRWeChatSDK.
+FOUNDATION_EXPORT const unsigned char MSRWeChatSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MSRWeChatSDK/PublicHeader.h>
+
+@import MSRWeChatScope;
